449240b62d148f8a19b77e95d7b96073780482aa
thouti <msg4srinu@gmail.com>
2018-12-21 01:08:00 +0530

    added boot startup code on 21-12-2018

--- src/main/java/com/mkyong/WelcomeController.java
+++ src/main/java/com/mkyong/WelcomeController.java
@@ -9,14 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
-	private String message = "Hello World";
-
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "welcome";
+		//model.put("message", this.message);
+		return "home";
 	}
 
 }
