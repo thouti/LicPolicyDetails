842ba9aa7c307c0be30673ee58085c7223773291
thouti <msg4srinu@gmail.com>
2019-03-24 00:09:46 +0530

    dto added

---
+++ src/main/java/com/policyholder/DTO/PolicyHolderDto.java
@@ -0,0 +1,48 @@
+package com.policyholder.DTO;
+
+public class PolicyHolderDto {
+	private String name;
+	private String policyNo;
+	private String mobileNo;
+	private String email;
+	private String premium;
+	private String mode;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPolicyNo() {
+		return policyNo;
+	}
+	public void setPolicyNo(String policyNo) {
+		this.policyNo = policyNo;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPremium() {
+		return premium;
+	}
+	public void setPremium(String premium) {
+		this.premium = premium;
+	}
+	public String getMode() {
+		return mode;
+	}
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+	
+
+}
